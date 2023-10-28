9a22b02d0841f0b47a16e2b03071b321d716f801
HEESUNae <heesun9456@gmail.com>
2023-10-28 12:42:33 +0900

    feat: vercel serverless

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,8 +4,14 @@ const hostname = window && window.location && window.location.hostname;
 
 const axiosApi = axios.create({
   baseURL: hostname === 'localhost' ? 'http://localhost:8888/' : 'https://react-sweet-potato.vercel.app/',
-  headers: { 'X-Custom-Header': 'foobar' },
-  timeout: 10000,
+  headers: {
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    'Access-Control-Allow-Headers':
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  },
+  timeout: 5000,
 });
 
 export default axiosApi;
