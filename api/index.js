21d10945d52337d62247b4f8045f001d41eeea97
HEESUNae <heesun9456@gmail.com>
2023-10-28 12:55:48 +0900

    fix: delete vercel api

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// /api/index.js
-import fetch from 'node-fetch';
-
-export default async function handler(request, response) {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data = await res.json();
-  return response.status(200).json({ data });
-}
